aef3494d1c4b614e96fc3159bb2da3daf26aae3f
Ot Vadim <v.otdelnov77@gmail.com>
2022-06-18 22:24:19 +0300

    my app in progress 18jun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/Button/MyButton";
+import MyInput from "./components/UI/Input/MyInput";
+import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'JavaScript', body:'Description'},
+      {id: 2, title: 'Java', body:'Description'},
+      {id: 3, title: 'Python', body:'Description'}
+  ])
+
+  /*const [posts2, setPosts2] = useState([
+    {id: 1, title: 'GoLang', body:'Description'},
+    {id: 2, title: 'Ruby', body:'Description'},
+    {id: 3, title: 'Kotlin', body:'Description'}
+  ])*/
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput type="text" placeholder="название постов"/>
+        <MyInput type="text" placeholder="описание поста"/>
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
+
+      <PostList posts={posts} title="Список постов 1"/>
+      
     </div>
   );
 }
